28c74210c7bfb646a4909fc6d007cdcf86a0e532
eff2d <effxmist@gmail.com>
2022-03-02 18:07:04 +0700

    Добавьте файлы проекта.

---
+++ test1.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream> 
+using namespace std;
+
+int main()
+{
+    setlocale(0, "");
+     int a, b; 
+    cout << "Введите первое число: ";
+    cin >> a; 
+    cout << "Введите второе число: ";
+    cin >> b;
+    /*12*/  int c = a + b; 
+    cout << "Сумма чисел = " << c << endl; 
+    return 0;
+}
