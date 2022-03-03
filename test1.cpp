982c3562680809f116c9205f5b420f95167b1f92
eff2d <effxmist@gmail.com>
2022-03-03 11:20:53 +0700

    +cycle summ + validation check

--- test1.cpp
+++ test1.cpp
@@ -4,16 +4,20 @@ using namespace std;
 int main()
 {
     setlocale(0, "");
+    int i;
+    int sum = 0;
     int a, b;
     cout << "Vvedite 1 chislo ";
     cin >> a;
     cout << "Vvedite 2 chislo ";
     cin >> b;
-    if (a < 10 && b < 10) {
-        int c = a + b;
-        cout << "summa =" << c << endl;
+    if (a > 0 && b <= 1000) {
+        for (i = a; i <= b; i++) {
+            sum = sum + i;
+        }
+        cout << "summa =" << sum << endl;
     }
-    else if (a > 10 || b > 10) {
+    else if (a <= 0 || b > 1000) {
         cout << "error error error alarm alarm alarm >:O" << endl;
     }
     return 0;
